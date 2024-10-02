a3bb9e5f1e80175b081e3b01d8b6b43a3b7b2b5e
lapaginadelarisa <lapaginadelarisa232@gmail.com>
2024-10-01 21:53:56 -0600

    Add files via upload

---
+++ js/nav-tab-fixed.js
@@ -0,0 +1,18 @@
+import { resaltaSiEstasEn } from "../lib/js/resaltaSiEstasEn.js"
+
+export class NavTabFixed extends HTMLElement {
+
+ connectedCallback() {
+  this.classList.add("md-tab", "fixed")
+
+  this.innerHTML = /* HTML */`
+   <a ${resaltaSiEstasEn(["/index.html", "", "/"])} href="index.html">
+    <span class="material-symbols-outlined">home</span>
+    Inicio
+   </a>
+  `
+ }
+
+}
+
+customElements.define("nav-tab-fixed", NavTabFixed)
